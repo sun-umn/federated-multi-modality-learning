759504d80a2509566ddb2a791fcac75e5d023481
PL97 <pengle@126.com>
2022-12-16 10:02:34 -0800

    specify model from name in init func

--- medical_ner/app/custom/simple_network.py
+++ medical_ner/app/custom/simple_network.py
@@ -1,14 +1,17 @@
+
 from transformers import BertForTokenClassification
-import torch.nn as nn
+import torch
 
-class BertModel(nn.Module):
+class BertModel(torch.nn.Module):
 
-    def __init__(self):
+    def __init__(self, num_labels, model_name='bert-base-uncased'):
 
         super(BertModel, self).__init__()
 
-        self.bert = BertForTokenClassification.from_pretrained('bert-base-cased', num_labels=19)
-
+        self.bert = BertForTokenClassification.from_pretrained(model_name, num_labels=num_labels, \
+                    output_attentions = False, \
+                    output_hidden_states = False)
+        
     def forward(self, input_id, mask, label):
 
         output = self.bert(input_ids=input_id, attention_mask=mask, labels=label, return_dict=False)
